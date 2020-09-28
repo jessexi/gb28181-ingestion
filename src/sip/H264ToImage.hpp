70853e15c34ec3260987b0189a975592ad1d3964
Jesse Xi <jj.xi@tianrang-inc.com>
2020-09-28 15:30:18 +0800

    support h264 to image

--- src/sip/H264ToImage.hpp
+++ src/sip/H264ToImage.hpp
@@ -1,18 +1,30 @@
 #ifndef H264_2_IMAGE_HPP
 #define H264_2_IMAGE_HPP
+#include <libavcodec/avcodec.h>
+#include <string>
+#include <iostream>
+
+#define INBUF_SIZE 4096
 
 class H264ToImage
 {
 private:
-    /* data */
+    const AVCodec *codec;
+    AVCodecParserContext *parser;
+    AVCodecContext *c = NULL;
+    AVPacket *pkt;
+    uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
+    uint8_t *data;
+    size_t data_size;
+    AVFrame *frame;
+
 public:
     H264ToImage(/* args */);
-
+    static void decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt,
+                   const char *filename);
     void init();
-    void save2Image(char* h264Buffer, int h264length, char *filename);
+    void save2Image(char *h264Buffer, int h264length, char *filename);
     ~H264ToImage();
 };
-
-
 
 #endif
